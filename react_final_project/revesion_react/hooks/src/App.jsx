0d08937ead8060afdbbd66b1bc22cb54f97de0c5
jatin-05dev <jatindev214@gmail.com>
2026-03-05 17:00:26 +0530

    hooks

---
+++ react_final_project/revesion_react/hooks/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState,useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+let [name,set]=useState("puju")
+let [num,setn]=useState(0)
+const [color, setColor] = useState("black");
+  // useEffect(()=>alert("cybrom...."),[])
+  // useEffect(()=>alert("cybrom...."))
+  useEffect(()=>alert("cybrom...."),[color])
+function inc(){
+set("puju hotty")
+}
+function un(){
+  setn(num++)
+}
+function dc(){
+  setn(num--)
+}
+  return (
+    <>
+
+      <h1 style={{ color: color }}>
+        React Color Change Component
+      </h1>
+    hello {name}  rating {num}
+    <button onClick={inc}>click to turn on puju </button>
+    <button onClick={()=>set("hotty rani puju")}>click  </button>
+    <button onClick={un}>click to rate puju</button>
+    <button onClick={dc}>click to rate puju</button>
+
+
+      <button onClick={() => setColor("red")}>Red</button>
+      <button onClick={() => setColor("green")}>Green</button>
+      <button onClick={() => setColor("blue")}>Blue</button>
+
+     </>
+  )
+}
+
+export default App
